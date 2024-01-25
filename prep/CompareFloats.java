410a8ec38adf6c2bb44bd6406335fb98e5435f50
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2024-01-25 13:48:23 -0600

    Create CompareFloats.java

---
+++ prep/CompareFloats.java
@@ -0,0 +1,11 @@
+package prep;
+
+public class CompareFloats {
+    public static void main(String[] args) {
+        double x = 1f/3f + 2f/3f;
+        double y = 1f/3f;
+        y += 2f/3f;
+        
+        System.out.println(x == y);
+    }
+}
