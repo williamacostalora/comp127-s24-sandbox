c08a269a7555a5d30f5136cdf8cca86806c595a5
Amin Alhashim <aalhashi@macalester.edu>
2024-01-26 09:23:38 -0600

    Try array instead of enum

--- prep/EnumeratedType.java
+++ prep/EnumeratedType.java
@@ -3,11 +3,18 @@ package prep;
 enum Grade {A, B, C, D, F}
 
 public class EnumeratedType {
+    public static final String[] Grade2 = {"A", "B", "C", "D", "F"};
+
     public static void main(String[] args) {
         Grade stu1Grade = Grade.A;
 
         System.out.println(stu1Grade);
         System.out.println(stu1Grade.ordinal());
         System.out.println(stu1Grade.name());
+
+        EnumeratedType.Grade2[0] = "Aminus";
+        EnumeratedType.Grade2 = {"Aminus"};
+        String stu2Grade = Grade2[0];
+        System.out.println(stu2Grade);
     }
 }
