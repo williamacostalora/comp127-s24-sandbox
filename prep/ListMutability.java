62be39970f772ad0898d79182b1bf870bb3ed545
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2024-01-29 07:31:48 -0600

    Rename things

--- prep/ListMutability.java
+++ prep/ListMutability.java
@@ -3,42 +3,41 @@ package prep;
 import java.util.List;
 import java.util.ArrayList;
 
-
 public class ListMutability {
     public static void main(String[] args) {
-        List<Integer> list1 = List.of(10, 20, 30);
-        // List<Integer> list1 = new ArrayList<>(List.of(10, 20, 30));
+        // List<Integer> accounts = List.of(10, 20, 30);
+        List<Integer> accounts = new ArrayList<>(List.of(10, 20, 30));
        
 
-        System.out.println("Before:\t" + list1);
-        System.out.println("Sum = " + sum(list1));
-        System.out.println("After:\t" + list1);
+        System.out.println("Before:\t" + accounts);
+        System.out.println("Sum = " + sum(accounts));
+        System.out.println("After:\t" + accounts);
         
         System.out.println();
         
-        System.out.println("Before:\t" + list1);
-        System.out.println("Sum = " + sumM(list1));
-        System.out.println("After:\t" + list1);
+        System.out.println("Before:\t" + accounts);
+        System.out.println("Sum = " + sumM(accounts));
+        System.out.println("After:\t" + accounts);
     }
 
 
     // sums the list and did not change its elements
-    public static int sum(List<Integer> list) {
+    public static int sum(List<Integer> accounts) {
         int sum = 0;
-        for (Integer item : list) {
-            sum += item;
+        for (Integer account : accounts) {
+            sum += account;
         }
 
         return sum;
     } 
 
-    
+
     // sum the list but changes its elements
-    public static int sumM(List<Integer> list) {
+    public static int sumM(List<Integer> accounts) {
         int sum = 0;
-        for(int i = 0; i<list.size(); i++) {
-            sum += list.get(i);
-            list.set(i, 0);
+        for(int i = 0; i<accounts.size(); i++) {
+            sum += accounts.get(i);
+            accounts.set(i, 0);
         }
 
         return sum;
