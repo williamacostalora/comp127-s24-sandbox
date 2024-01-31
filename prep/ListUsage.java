2fee54fbdfe3385f60f6416944cb973ae538b28b
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2024-01-30 22:04:56 -0600

    Update ListUsage.java

--- prep/ListUsage.java
+++ prep/ListUsage.java
@@ -7,12 +7,12 @@ public class ListUsage {
     public static void main(String[] args) {
         List<String> list1 = List.of("AA", "BB", "CC");
         
-        // using for
+        // using normal for, also know as c-style for
         for(int i=0; i<list1.size(); i++) {
             System.out.println(list1.get(i));
         }
 
-        // using for-each
+        // using for-each, also know as enhanced for
         for (String item : list1) {
             System.out.println(item);
         }
