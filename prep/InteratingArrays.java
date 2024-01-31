e15d71bfbe6f6a0a585be58d292b1d47d7d12cfa
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2024-01-30 23:29:56 -0600

    Create InteratingArrays.java

---
+++ prep/InteratingArrays.java
@@ -0,0 +1,28 @@
+package prep;
+
+import java.util.List;
+import java.lang.reflect.Array;
+import java.util.Iterator;
+
+public class InteratingArrays {
+    public static void main(String[] args) {
+        int[] nums = {10, 20, 30};
+        System.out.println((Object) nums instanceof Iterable);
+
+        /* loop */
+        // for(int i=0; i<nums.length; i++) {
+        //     System.out.println(nums[i]);
+        // }
+
+        /* for-each */
+        // for (Integer num : nums) {
+        //     System.out.println(num);
+        // }
+
+        /* hasNext/next */
+        // Iterator<Integer> itr = nums.iterator();
+        // while(itr.hasNext()) {
+        //     System.out.println(itr.next());
+        // }
+    }
+}
