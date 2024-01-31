cb2aec42b61f59e0a69a6b991ce161cca9fb45d6
Amin Alhashim <aalhashi@macalester.edu>
2024-01-31 09:19:00 -0600

    Update WhileVsDo.java

--- Lec06/WhileVsDo.java
+++ Lec06/WhileVsDo.java
@@ -5,6 +5,21 @@ package Lec06;
  */
 public class WhileVsDo {
     public static void main(String[] args) {
+        final int SENTINAL = 2;
+        int i = 0;
+
+        System.out.println("--- WHILE ---");
+        while(i<SENTINAL) {
+            i++;
+            System.out.println(i);
+        }
         
+
+        System.out.println("--- DO ---");
+        i = 0;
+        do {
+            i++;
+            System.out.println(i);
+        } while(i<SENTINAL);
     }
 }
