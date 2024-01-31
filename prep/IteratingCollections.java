8ce1cf674f9a3bb10093e2c93b8bcbac46baaef8
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2024-01-30 23:29:53 -0600

    Create IteratingCollections.java

---
+++ prep/IteratingCollections.java
@@ -0,0 +1,27 @@
+package prep;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class IteratingCollections {
+    public static void main(String[] args) {
+        List<Integer> nums = List.of(10, 20, 30);
+        // System.out.println(nums instanceof Iterable);
+
+        /* loop */
+        // for(int i=0; i<nums.size(); i++) {
+        //     System.out.println(nums.get(i));
+        // }
+
+        /* for-each */
+        // for (Integer num : nums) {
+        //     System.out.println(num);
+        // }
+
+        /* hasNext/next */
+        // Iterator<Integer> itr = nums.iterator();
+        // while(itr.hasNext()) {
+        //     System.out.println(itr.next());
+        // }
+    }
+}
