a640e1a23547f08858342e58297142f942598173
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2024-02-04 23:05:08 -0600

    Add kilt-graphics example

---
+++ Graphics/RandomGraphics.java
@@ -0,0 +1,13 @@
+import edu.macalester.graphics.*;
+import edu.macalester.graphics.ui.Button;
+
+public class RandomGraphics {
+    public static void main(String[] args) {
+        CanvasWindow window = new CanvasWindow("Test", 100, 100);
+
+        Button b = new Button("Click Me!");
+        b.onClick(() -> b.moveBy(1, 1));
+        window.add(b);
+        
+    }
+}
