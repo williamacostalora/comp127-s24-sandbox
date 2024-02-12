4eeee697890e99035e003988ae0b568e44e0c305
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2024-02-12 11:07:55 -0600

    Lec11 demo

---
+++ src/Lab11/VoidMethod.java
@@ -0,0 +1,10 @@
+package Lab11;
+
+public class VoidMethod {
+    public static void something(int x) {
+        if(x < 0) return;
+        if(x > 10) return;
+
+        System.out.println("Hello");
+    }
+}
