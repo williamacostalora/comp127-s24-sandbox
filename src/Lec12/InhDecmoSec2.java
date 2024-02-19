46f8461c51d1a15ab34000d08d9db19bf51b6a83
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2024-02-19 11:58:27 -0600

    Lec12 demo for sec2

---
+++ src/Lec12/InhDecmoSec2.java
@@ -0,0 +1,44 @@
+package Lec12;
+
+public class InhDecmoSec2 {
+    public static void main(String[] args) {
+        P[] ps = new P[3];
+        ps[0] = new P();
+        ps[1] = new C();
+        ps[2] = new CC();
+
+        for (int i = 0; i < ps.length; i++) {
+            ps[i].prt();
+        }
+
+        // P p = new P();
+        // p.prt();
+
+        // P c = new C();
+        // c.prt();
+
+        // P cc = new CC();
+        // cc.prt();
+    }
+}
+
+class P {
+    int p;
+
+    void prt() {
+        System.out.println("P");
+    }
+}
+
+class C extends P {
+    int c;
+
+    void prt() {
+        super.prt();
+        System.out.println("C");
+    }
+}
+
+class CC extends C {
+    int cc;
+}
