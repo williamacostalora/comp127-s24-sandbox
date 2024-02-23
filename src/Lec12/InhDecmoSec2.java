61b08a99ca528eccb7e84e73261004e75541ec15
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2024-02-22 23:07:01 -0600

    Commented to make Gradle happy

--- src/Lec12/InhDecmoSec2.java
+++ src/Lec12/InhDecmoSec2.java
@@ -1,44 +1,44 @@
-package Lec12;
+// package Lec12;
 
-public class InhDecmoSec2 {
-    public static void main(String[] args) {
-        P[] ps = new P[3];
-        ps[0] = new P();
-        ps[1] = new C();
-        ps[2] = new CC();
+// public class InhDecmoSec2 {
+//     public static void main(String[] args) {
+//         P[] ps = new P[3];
+//         ps[0] = new P();
+//         ps[1] = new C();
+//         ps[2] = new CC();
 
-        for (int i = 0; i < ps.length; i++) {
-            ps[i].prt();
-        }
+//         for (int i = 0; i < ps.length; i++) {
+//             ps[i].prt();
+//         }
 
-        // P p = new P();
-        // p.prt();
+//         // P p = new P();
+//         // p.prt();
 
-        // P c = new C();
-        // c.prt();
+//         // P c = new C();
+//         // c.prt();
 
-        // P cc = new CC();
-        // cc.prt();
-    }
-}
+//         // P cc = new CC();
+//         // cc.prt();
+//     }
+// }
 
-class P {
-    int p;
+// class P {
+//     int p;
 
-    void prt() {
-        System.out.println("P");
-    }
-}
+//     void prt() {
+//         System.out.println("P");
+//     }
+// }
 
-class C extends P {
-    int c;
+// class C extends P {
+//     int c;
 
-    void prt() {
-        super.prt();
-        System.out.println("C");
-    }
-}
+//     void prt() {
+//         super.prt();
+//         System.out.println("C");
+//     }
+// }
 
-class CC extends C {
-    int cc;
-}
+// class CC extends C {
+//     int cc;
+// }
--- src/Lec12/InhDemoSec1.java
+++ src/Lec12/InhDemoSec1.java
@@ -1,35 +1,35 @@
-package Lec12;
+// package Lec12;
 
-public class InhDemoSec1 {
-    public static void main(String[] args) {
-        // P p = new P();
-        // p.prnt();
+// public class InhDemoSec1 {
+//     public static void main(String[] args) {
+//         // P p = new P();
+//         // p.prnt();
 
-        P c = new C();
-        c.prnt();
+//         P c = new C();
+//         c.prnt();
 
-        P cc = new CC();
-        cc.prnt();
+//         P cc = new CC();
+//         cc.prnt();
 
-    }}
+//     }}
 
-class P {
-    int p;
-    void prnt() {System.out.println("P");}
-}
+// class P {
+//     int p;
+//     void prnt() {System.out.println("P");}
+// }
 
-class C extends P {
-    int c;
-    void prnt() {
-        super.prnt();
-        System.out.println("C");
-    }
-}
+// class C extends P {
+//     int c;
+//     void prnt() {
+//         super.prnt();
+//         System.out.println("C");
+//     }
+// }
 
-class CC extends C {
-    int cc;
-    void prnt() {
-        super.prnt();
-        System.err.println("CC");
-    }
-}
+// class CC extends C {
+//     int cc;
+//     void prnt() {
+//         super.prnt();
+//         System.err.println("CC");
+//     }
+// }
