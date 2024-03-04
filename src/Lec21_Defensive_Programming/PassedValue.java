153ec5905bcae89056fc7c2b3282dde01a73b887
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2024-03-04 11:14:05 -0600

    Update PassedValue.java

--- src/Lec21_Defensive_Programming/PassedValue.java
+++ src/Lec21_Defensive_Programming/PassedValue.java
@@ -5,7 +5,7 @@ public class PassedValue {
         int sum = 0;
         for(int i=0; i<array.length; i++) {
             sum += array[i];
-            array[i] = 100;
+            array[i] = array[i]*2;
         }
 
         return sum;
