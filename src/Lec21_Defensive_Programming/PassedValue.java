7c9e0f7f2d86fa0248a05938f6570410f33dbf20
Amin Alhashim <aalhashi@macalester.edu>
2024-03-04 09:33:40 -0600

    Create PassedValue.java

---
+++ src/Lec21_Defensive_Programming/PassedValue.java
@@ -0,0 +1,22 @@
+package Lec21_Defensive_Programming;
+
+public class PassedValue {
+    public static int sumArray(int[] array) {
+        int sum = 0;
+        for(int i=0; i<array.length; i++) {
+            sum += array[i];
+            array[i] = 100;
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        int sum = sumArray(arr);
+        System.out.println(sum);
+
+        sum = sumArray(arr);
+        System.out.println(sum);
+    }
+}
