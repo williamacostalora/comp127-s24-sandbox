ecfee2239000a6fdfe826dc8fcf8a0630c07889d
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2024-04-01 06:28:37 -0500

    Exam2

---
+++ src/test2/Arrays.java
@@ -0,0 +1,11 @@
+package test2;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] a = new int[10];
+
+        for(int i : a) {
+            System.out.println(i);
+        }
+    }
+}
