f6112b56744f56162a79dc5c44d79e087049db4f
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2024-04-05 08:10:00 -0500

    Update Arrays.java

--- src/test2/Arrays.java
+++ src/test2/Arrays.java
@@ -2,10 +2,18 @@ package test2;
 
 public class Arrays {
     public static void main(String[] args) {
-        int[] a = new int[10];
+        // int[] a = new int[10];
 
-        for(int i : a) {
-            System.out.println(i);
+        // for(int i : a) {
+        //     System.out.println(i);
+        // }
+
+        acc("1", "2", "3", "4", "5");
+    }
+
+    static void acc(String ... arr) {
+        for(String s : arr) {
+            System.out.println(s);
         }
     }
 }
