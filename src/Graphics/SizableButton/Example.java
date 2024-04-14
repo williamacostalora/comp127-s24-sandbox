0351c774b9495504f4a1d39070a3a106e27a4325
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2024-04-14 13:06:51 -0500

    Update Example.java

--- src/Graphics/SizableButton/Example.java
+++ src/Graphics/SizableButton/Example.java
@@ -4,8 +4,9 @@ import edu.macalester.graphics.CanvasWindow;
 
 public class Example {
     public static void main(String[] args) {
-        CanvasWindow window = new CanvasWindow("Big Button", 100, 100);
-        Button jb = new Button("Test", 50, 50);
+        CanvasWindow window = new CanvasWindow("Big Button", 400, 400);
+        Button jb = new Button("Test", 200, 200);
+        jb.setPosition(100, 100);
         jb.onClick(() -> jb.setPosition(jb.getX() + 2, jb.getY() + 2));
 
         window.add(jb);
